f731c66e8bee13098050bf9b812e1073c5b77b3d
Ivan <isakin89@gmail.com>
2024-11-07 13:00:21 +0200

    fix from feedback

--- src/main/java/com/comparus/assessment/ClientServiceApplication.java
+++ src/main/java/com/comparus/assessment/ClientServiceApplication.java
@@ -3,10 +3,10 @@ package com.comparus.assessment;
 import com.comparus.assessment.configuration.DatasourceProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.data.jdbc.JdbcRepositoriesAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
-@SpringBootApplication(exclude = {JdbcRepositoriesAutoConfiguration.class})
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 @EnableConfigurationProperties(DatasourceProperties.class)
 public class ClientServiceApplication {
 
